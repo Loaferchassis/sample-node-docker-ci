fac0841957f1f2974408ea4a76e39cd312508d3f
loaferchassis <loaferchassis@gmail.com>
2017-10-10 23:31:56 +0300

    EOF fixed

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ docker rm web || true
 docker rmi alexanderd/sample-node:current || true
 docker tag alexanderd/sample-node:latest alexanderd/sample-node:current
 docker run -d --net app --restart always --name web -p 80:80 alexanderd/sample-node:current
-EOF>>
+EOF
