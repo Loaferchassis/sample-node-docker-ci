dd3bfa670d5e749d998a05834832a980cde9df9a
loaferchassis <loaferchassis@gmail.com>
2017-10-10 23:28:19 +0300

    EOF added

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 docker push alexanderd/sample-node
 
-ssh loaferchassis@35.195.183.2 
+ssh loaferchassis@35.195.183.2 <<EOF
 docker pull alexanderd/sample-node:latest
 docker stop web || true
 docker rm web || true
 docker rmi alexanderd/sample-node:current || true
 docker tag alexanderd/sample-node:latest alexanderd/sample-node:current
 docker run -d --net app --restart always --name web -p 80:80 alexanderd/sample-node:current
-
+EOF>>
