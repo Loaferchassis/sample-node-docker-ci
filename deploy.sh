51e4a80224eb68e7ea5ca77f151653516e729796
loaferchassis <loaferchassis@gmail.com>
2017-10-10 23:40:05 +0300

    deploy fixed

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,5 @@ docker stop web || true
 docker rm web || true
 docker rmi alexanderd/sample-node:current || true
 docker tag alexanderd/sample-node:latest alexanderd/sample-node:current
-docker run -d --net app --restart always --name web -p 80:80 alexanderd/sample-node:current
+docker run -d --restart always --name web -p 80:80 alexanderd/sample-node:current
 EOF
