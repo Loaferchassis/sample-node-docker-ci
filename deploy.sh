1e5d29cb546fd1009ca47cc8f48ed1221ab8db74
loaferchassis <loaferchassis@gmail.com>
2017-10-10 21:46:04 +0300

    smth2

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@
 docker push alexanderd/sample-node
 
 ssh deploy@35.195.183.2 
-docker pull alexanded/sample-node:latest
+docker pull alexanderd/sample-node:latest
 docker stop web || true
 docker rm web || true
-docker rmi alexanded/sample-node:current || true
+docker rmi alexanderd/sample-node:current || true
 docker tag alexanderd/sample-node:latest alexanderd/sample-node:current
 docker run -d --net app --restart always --name web -p 80:80 alexanderd/sample-node:current
 
